43b6c5faa6fc4346870a095160af90211ed0e28b
MinhTuannguyengtranminh <my1234567890.tuan@gmail.com>
2024-04-02 10:07:48 +0700

    test

---
+++ python nhân 1 số từ 1 đến 100.py
@@ -0,0 +1,8 @@
+#Bangcuuchuong = input("Bạn muốn bảng cửu chương mấy?")
+#print(Bangcuuchuong)
+#for x in range(1,11):
+   # print (Bangcuuchuong, " x ", x ," = ",int(Bangcuuchuong ) * x)
+
+for n in range(2,10):
+    for x in range(1,101):
+        print (n, " x ", x ," = ",int(n) * x)
